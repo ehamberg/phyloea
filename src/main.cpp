e17ae40aff267b24d00d750912ca3d355e6cc1c5
Erlend Hamberg <ehamberg@gmail.com>
2010-11-18 20:14:37 +0100

    test program

--- src/main.cpp
+++ src/main.cpp
@@ -16,62 +16,30 @@ int main(int argc, const char *argv[])
 {
     srand(time(NULL));
 
-    //PhyloTreeNode* n1 = new PhyloTreeNode("species 1", "AA");
+    vector<PhyloTreeNode*> nodes = Fasta::readFastaFile("tests/aligned.fasta", true);
+    PhyloTree t;
+    t.buildRandomTree(nodes);
+    cout << t.dot() << '\n';
+    cerr << PhyloTreeNode::prefixRepresentation(t.getRoot()) << '\n';
 
-    //PhyloTreeNode* n2 = new PhyloTreeNode("species 2", "CC");
-
-    //PhyloTreeNode* n3 = new PhyloTreeNode("species 3", "CC");
-
-    //PhyloTreeNode* n4 = new PhyloTreeNode("species 4", "CC");
-
-    //PhyloTreeNode* n5 = new PhyloTreeNode("species 5", "GG");
-
-    //PhyloTreeNode* y = new PhyloTreeNode;
-    //y->addChild(n1, 0.5);
-    //y->addChild(n2, 0.5);
-
-    //PhyloTreeNode* w = new PhyloTreeNode;
-    //w->addChild(n4, 0.5);
-    //w->addChild(n5, 0.5);
-
-    //PhyloTreeNode* z = new PhyloTreeNode;
-    //z->addChild(n3, 0.5);
-    //z->addChild(w, 0.5);
-
-    //PhyloTreeNode* x = new PhyloTreeNode;
-    //x->addChild(y, 0.5);
-    //x->addChild(z, 0.5);
-
-    //PhyloTree t(x, new Kimura(10));
-
-    //cout << pow(10, t.logLikelihood()) << '\n';
-    //cout << t.dot() << '\n';
-
-    //vector<PhyloTreeNode*> nodes = Fasta::readFastaFile("tests/aligned.fasta");
-
-    //vector<PhyloTreeNode*>::iterator it;
-    //for (it = nodes.begin(); it != nodes.end(); ++it) {
-    //    delete *it;
+    //vector<string> randomPop;
+    //for (int i = 0; i < 10; i++) {
+    //    string s;
+    //    for (int j = 0; j < 20; j++) {
+    //        s += '0';//(char)(rand()%2+(int)'0');
+    //    }
+    //    randomPop.push_back(s);
     //}
 
-    vector<string> randomPop;
-    for (int i = 0; i < 10; i++) {
-        string s;
-        for (int j = 0; j < 20; j++) {
-            s += (char)(rand()%2+(int)'0');
-        }
-        randomPop.push_back(s);
-    }
-
-    EASystem<string> testEA(new MutateString(0.1), new RecombineString(0.7), new RouletteWheelSelection<string>, new PipeFitnessFunc<string>);
-    //testEA.setLogStream(&cerr);
+    //EASystem<string> testEA(new MutateString(0.01), new RecombineString(0.7), new RouletteWheelSelection<string>, new PipeFitnessFunc<string>);
+    ////testEA.setLogStream(&cerr);
     //testEA.setElitism(4);
-    testEA.setDebugging(true);
-    testEA.setPopulation(randomPop);
-    testEA.runGenerations(10);
+    //testEA.setDebugging(true);
+    //testEA.setPopulation(randomPop);
+    //testEA.runGenerations(1);
 
-    cerr << "Final generation:\n";
-    testEA.exportGenomes(cerr);
+    //cerr << "Final generation:\n";
+    //testEA.exportGenomes(cerr);
 
     return 0;
 }
