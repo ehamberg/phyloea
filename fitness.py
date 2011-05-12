9887c98b636b0c211e9fad9f30081ca96b6267ff
Erlend Hamberg <ehamberg@gmail.com>
2011-05-12 12:35:25 +0200

    --debug switch for fitness.py

--- fitness.py
+++ fitness.py
@@ -14,6 +14,7 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 lengths = not (len(sys.argv) == 3 and sys.argv[2] == "--no-lengths")
+debug = len(sys.argv) == 3 and sys.argv[2] == "--debug"
 
 line = sys.stdin.readline()
 while line:
@@ -24,8 +25,11 @@ while line:
     tree_file.write(newick_data)
     tree_file.flush()
 
-    args = ["phyml", "-i", sys.argv[1], "-m", "K80", "-f",
-            "0.25,0.25,0.25,0.25", "-t", "10.0", "-u", tree_file.name, "-o",
+    if debug:
+        sys.stderr.write(str(newick_data)+'\n')
+
+    args = ["phyml", "-b", "0", "-i", sys.argv[1], "-m", "K80", "-f",
+            "0.25,0.25,0.25,0.25", "-t", "4.0", "-u", tree_file.name, "-o",
             ("n" if lengths else "l")]
 
     start = time.time()
@@ -34,10 +38,10 @@ while line:
     m = re.search('Log likelihood of the current tree:\s+(-?\d+\.\d+)', output)
     logLH = m.group(1)
 
-    sys.stderr.write(str(newick_data)+': '+logLH+'('+str(time.time()-start)+' s)\n')
+    #sys.stderr.write(str(newick_data)+': '+logLH+'('+str(time.time()-start)+' s)\n')
 
     print logLH
 
     line = sys.stdin.readline()
 
-sys.stderr.write("done\n")
+sys.stderr.write("done.\n")
